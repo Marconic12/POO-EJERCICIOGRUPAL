96bbd9088ee1a2699dcc8cb4049e65ad1d7faac3
Marconic12 <masalas@uamv.edu.ni>
2026-03-22 09:30:04 -0600

    solo cambie los nombres y elimine archivos innecesarios

--- src/run/Ejercicio1.java
+++ src/run/Ejercicio1.java
@@ -1,9 +1,36 @@
 package run;
 
-public class Ejercicio1
-{
-    public static void main(String[] args)
-    {
+import javax.swing.*;
 
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        double peso;
+        int distancia;
+        String esFragil;
+        String msn = "";
+
+        peso = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el peso del paquete (kg):"));
+        distancia = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la distancia del envío (km):"));
+        esFragil = JOptionPane.showInputDialog("El producto es frágil? (si/no):");
+
+        msn = "Descripcion de envio:\n";
+        msn += "Peso: " + peso + " kg\n";
+        msn += "Distancia: " + distancia + " km\n";
+
+
+        if (peso > 50 && distancia > 100) {
+            msn += "Su envio sera: 'Transporte pesado'.";
+        }
+        else if (peso < 50 && esFragil.equalsIgnoreCase("si")) {
+            msn += "Su envio sera: 'Envío Premium'.";
+        }
+        else if (peso < 10 && distancia < 20) {
+            msn += "Su envio sera: 'Dron'.";
+        }
+        else {
+            msn += "Su envio sera: 'Envío Estándar'.";
+        }
+
+        JOptionPane.showMessageDialog(null, msn);
     }
 }
